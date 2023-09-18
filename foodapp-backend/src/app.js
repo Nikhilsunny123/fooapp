0fd977d731706785f94caa0a7906fbc0283e49c7
Nikhilsunny123 <nikhilsunny35@gmail.com>
2023-09-18 08:46:52 +0530

    feat: My first feature

---
+++ foodapp-backend/src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db";
+import bodyParser from "body-parser";
+ import foodAppRouter from "./routes/foodAppRouter";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("yo");
+});
+
+app.use(bodyParser.json());
+ app.use("/foodapp", foodAppRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
+
+// process.on("uncaughtException", function (err) {
+//   console.log("Caught exception: ", err);
+// });
